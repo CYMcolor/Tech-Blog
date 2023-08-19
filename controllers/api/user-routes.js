a067c7f1e9a1d3a9318639aa9af5ca186c55a59c
Camille MacLaren <camillemaclaren@gmail.com>
2023-08-19 15:38:52 -0500

    add logout to api/user-routes
    changed variable names

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -7,9 +7,9 @@ router.post('/', async (req, res) => {
       const userData = await User.create(req.body);
   
       req.session.save(() => {
-        req.session.user_id = userData.id;
+        req.session.userId = userData.id;
         // if signed up session is logged in
-        req.session.logged_in = true;
+        req.session.loggedIn = true;
   
         res.status(200).json(userData);
       });
@@ -39,12 +39,24 @@ router.post('/login', async (req, res) => {
         }
         //save session
         req.session.save(() =>{
-            req.session.user_id = userData.id;
-            req.session.logged_in = true;
+            req.session.userId = userData.id;
+            req.session.loggedIn = true;
             res.json({ user: userData, message: 'You are now logged in!' });
         });
     } catch (err) {
         res.status(400).json(err);
+    }
+});
+
+// logout
+router.post('logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            // 204 means that the req sucess but there is not content
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
     }
 });
 
