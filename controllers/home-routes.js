84faa313e04f69817fa3c1373fc95e3b5bd35472
Camille MacLaren <camillemaclaren@gmail.com>
2023-08-18 23:11:00 -0500

    start of home-routes and made helper.js

---
+++ controllers/home-routes.js
@@ -0,0 +1,6 @@
+//dependcies
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+const router = require('express').Router();
+
+module.exports = router;
