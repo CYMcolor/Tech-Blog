cb974cb7484c7044861607d91818e7e49f26e2b6
Camille MacLaren <camillemaclaren@gmail.com>
2023-08-19 01:03:09 -0500

    Home page now renders all posts
    create home handlebars
    small css update

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,10 +3,22 @@ const sequelize = require('../config/connection');
 const { User, Post, Comment } = require('../models');
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     try {
+        //get all posts
+        const postData = await Post.findAll({
+            include: [
+                {
+                  model: User,
+                  attributes: ['username'],
+                },
+            ]
+        });
+        //serialize the data 
+        const posts = postData.map((post) => post.get({plain: true}));
         // render the homp view
-        res.render('home', { 
+        res.render('home', {
+            posts, 
             logged_in: req.session.logged_in 
         });
     } catch (err) {
