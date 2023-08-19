86733b6de805c4638d71d6956626a9cd38016ab6
Camille MacLaren <camillemaclaren@gmail.com>
2023-08-19 01:40:09 -0500

    Add single post page
    when clicking on title user will be redirected to single page
    still need to add comment section

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -26,6 +26,32 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/post/:id', async (req, res) => {
+    try {
+        //get a singular post
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                  model: User,
+                  attributes: ['username'],
+                },
+                // {
+                //   model: Comment,
+                //   attributes: ['username', 'content', 'create_at'],  
+                // }
+            ]
+        });
+        const post = postData.get({plain: true});
+        // render the homp view
+        res.render('single-post', {
+            post, 
+            logged_in: req.session.logged_in 
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 router.get('/login', (req, res) => {
     //if logged in redirect to home (temporary)
     if (req.session.logged_in) {
