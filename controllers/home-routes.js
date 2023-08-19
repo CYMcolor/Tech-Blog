d894fe969d568f30f9afef2cf4862dcbaafe3e3a
Camille MacLaren <camillemaclaren@gmail.com>
2023-08-18 23:28:54 -0500

    first sign of handlebars working with routes
    add basic route to render homepage
    fix if else statement in main layout in views

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,4 +3,11 @@ const sequelize = require('../config/connection');
 const { User, Post, Comment } = require('../models');
 const router = require('express').Router();
 
+router.get('/', (req, res) => {
+    // render 
+    res.render('home', { 
+        logged_in: req.session.logged_in 
+    });
+
+});
 module.exports = router;
