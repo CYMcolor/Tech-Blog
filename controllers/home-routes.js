0a5453e398c481639891bdebfbe7b40da1561984
Camille MacLaren <camillemaclaren@gmail.com>
2023-08-19 00:19:34 -0500

    create login handlebars and route
    does not have working java script for form submission yet

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,10 +4,23 @@ const { User, Post, Comment } = require('../models');
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    // render 
-    res.render('home', { 
-        logged_in: req.session.logged_in 
-    });
-
+    try {
+        // render the homp view
+        res.render('home', { 
+            logged_in: req.session.logged_in 
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
+
+router.get('/login', (req, res) => {
+    //if logged in redirect to home (temporary)
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
 module.exports = router;
