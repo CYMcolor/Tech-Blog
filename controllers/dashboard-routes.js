b046d4a9eb3a90ec77fef2219d7565f2fbc10bae
Camille MacLaren <camillemaclaren@gmail.com>
2023-08-19 19:32:07 -0500

    dashboard routes were added
    need to add authentication

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,31 @@
+//dependcies
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+const router = require('express').Router();
+
+router.get('/', async (req, res) => {
+    try {
+        //get all posts
+        const postData = await Post.findAll({
+            where:{
+                user_id: req.session.user_id
+            },
+            include: [
+                {
+                  model: User
+                },
+            ]
+        });
+        //serialize the data 
+        const posts = postData.map((post) => post.get({plain: true}));
+        // render the homp view
+        res.render('dashboard', {
+            posts, 
+            logged_in: req.session.logged_in 
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
