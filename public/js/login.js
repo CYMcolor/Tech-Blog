c57b6442c5c8d8270173a407f567359ee9c259d5
Camille MacLaren <camillemaclaren@gmail.com>
2023-08-20 20:28:40 -0500

    add sign up function

--- public/js/login.js
+++ public/js/login.js
@@ -6,13 +6,13 @@ const loginFormHandler =  async (event) => {
     const password = document.querySelector('#password-login').value.trim();
     // if both fields are there
     if (username && password) {
-        // send post request to API user
+        // send post request to API user for (session)
         const response = await fetch('api/users/login',{
             method: 'POST',
             body: JSON.stringify({username, password}),
             headers: { 'Content-Type': 'application/json'},
         });
-        // if sucessful redirect user to dashboard (not avaibile currently)
+        // if sucessful redirect user to dashboard
         if(response.ok){
             document.location.replace('/dashboard');
         } else {
@@ -21,6 +21,39 @@ const loginFormHandler =  async (event) => {
     } 
 };
 
+// signup handler
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector('#username-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+  const repassword = document.querySelector('#re-password-signup').value.trim();
+
+  if (password != repassword){
+    alert("Passwords does not match")
+  } else if(password && repassword && username){
+    //send an api POST request to users
+    const response = await fetch('api/users/',{
+        method: 'POST',
+        body: JSON.stringify({username, password}),
+        headers: { 'Content-Type': 'application/json'},
+    });
+
+    // if sucessful redirect user to dashboard
+    if(response.ok){
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+  }
+};
+
+// login event
 document
   .querySelector('.login-form')
   .addEventListener('submit', loginFormHandler);
+
+// signup event
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
